bb56d787d4ad7f57b8452b8ed8e040721691243e
Sergey Kireev <de-griiz@yandex.ru>
2021-11-25 22:33:56 +0500

    Задание 3

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,23 @@
+const button = document.querySelector('.dropdown__value');
+const list = document.querySelector('.dropdown__list');
+const links = document.querySelectorAll('a');
+
+button.addEventListener('click', show);
+
+function show() {
+
+    if (list.className === 'dropdown__list') {
+        list.className = 'dropdown__list dropdown__list_active';
+    } else {
+        list.className = 'dropdown__list';
+    }
+}
+
+for (let link of links) {
+    link.addEventListener('click', rename);
+}
+
+function rename(event) {
+    event.preventDefault();
+    button.textContent = event.target.textContent;
+}
